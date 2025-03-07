80fd84e6f36ff994b2f40e4a9784e98440550921
owenprlce <158511754+owenprlce@users.noreply.github.com>
2025-03-07 13:20:00 -0600

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import JobList from "./components/JobList"; // Ensure correct path
+function App() {
+  return (
+      <div>
+        <h1>Employee Management</h1>
+        <JobList />
+      </div>
+  );
+}
+export default App;
